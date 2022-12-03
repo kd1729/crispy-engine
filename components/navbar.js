084406c4579f02b69497d42dd3f2b6a9ccd76b09
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-03 17:43:45 +0530

    added links and styling

--- components/navbar.js
+++ components/navbar.js
@@ -1,14 +1,46 @@
 import Link from "next/link";
 
+const navigationRoutes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+  {
+    name: "Skills",
+    path: "/skills",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Experience",
+    path: "/experience",
+  },
+  {
+    name: "Blogs",
+    path: "/blogs",
+  },
+];
+
 function NavBar() {
   return (
-    <div className="bg-slate-500">
-      <Link href="/home">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/skills">Skills</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/experience">Experience</Link>
-      <Link href="/blogs">Blogs</Link>
+    <div className="text-xl bg-emerald-800 flex justify-end">
+      {navigationRoutes.map((route) => (
+        <Link key={route.name} href={route.path} passHref>
+          <div className="text-gray-50 m-4 hover:bg-emerald-700">
+            {route.name}
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
