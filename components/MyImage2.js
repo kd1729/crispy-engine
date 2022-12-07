0f845befec19b6a648a28cc301f4981d79dac280
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-07 19:43:04 +0530

    removed text from image

--- components/MyImage2.js
+++ components/MyImage2.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function MyImage2() {
   return (
     <div className="photo">
-        <h1>Kaustubh Dwivedi</h1>
+        {/* <h1>Kaustubh Dwivedi</h1> */}
         <Image className="img" src="/myself.png" alt="Kaustubh Dwivedi" height={300} width={300} />
         <div className="glow-wrap">
           <i className="glow"></i>
