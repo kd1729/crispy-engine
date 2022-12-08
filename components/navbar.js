222c687c64688ba2ccfa24cf248ea02038a9e3ac
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-08 23:01:45 +0530

    fixed navbar elements' width

--- components/navbar.js
+++ components/navbar.js
@@ -5,26 +5,32 @@ const navigationRoutes = [
   {
     name: "Home",
     path: "/",
+    width: "w-12",
   },
   {
     name: "About",
     path: "/about",
+    width: "w-12",
   },
   {
     name: "Projects",
     path: "/projects",
+    width: "w-16",
   },
   {
     name: "Experiences",
     path: "/experience",
+    width: "w-24",
   },
   {
     name: "Blogs",
     path: "/blogs",
+    width: "w-10",
   },
   {
     name: "Contact",
     path: "/contact",
+    width: "w-20",
   },
 ];
 
@@ -35,12 +41,12 @@ function NavBar({ activeSection }) {
         <Link key={route.name} href={route.path} passHref>
           <div
             className={
-              route.name === activeSection
-                ? "text-[#247fff] underline text-2xl m-4"
-                : "text-white text-lg m-4"
+              [route.name === activeSection
+                ? "text-[#247fff] underline underline-offset-4 m-4"
+                : "text-white", "text-lg m-4 hover:text-[#247fff]"].join(" ")
             }
           >
-            <div className="hover:font-bold w-28 font-semibold ease-in-out duration-300">
+            <div className= { [route.width, "font-semibold ease-in-out duration-300"].join(" ")}>
               {route.name}
             </div>
           </div>
