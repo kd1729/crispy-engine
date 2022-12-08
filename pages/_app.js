800feea19106cf5675aa6b0d655e9c1d81399b51
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-08 20:29:10 +0530

    Contact Me Stage 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/myname.css'
 import "../styles/aboutme.css"
+import "../styles/contactme.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
