ef28fe0e76cb20d48518f4d77593d21e2623c8f6
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-09 16:32:51 +0530

    added linkedin button

--- components/ResumeDownload.js
+++ components/ResumeDownload.js
@@ -1,12 +1,11 @@
 export default function ResumeDownload() {
   return (
     <div
-      className="flex flex-row justify-center mt-20 ml-80 w-max text-lg bg-white px-5 py-3 rounded-lg text-emerald-600 font-bold
-               shadow-[0_10px_20px_-15px_#ffffffff] hover:shadow-[0_10px_20px_-15px_#000000ff]
+      className="flex flex-row justify-center mt-20 ml-80 w-max text-lg bg-white px-5 py-3 rounded-lg text-emerald-700 font-bold
+                hover:shadow-[0_10px_20px_-15px_#ffffffff] shadow-[0_10px_20px_-15px_#000000ff]
                 ease-in-out duration-300
-                hover:text-xl hover:cursor-pointer hover:text-emerald-500 hover:ml-[400px] hover:mt-[80px]
+                hover:text-xl hover:cursor-pointer hover:text-emerald-600 hover:ml-[390px] hover:mt-[100px]
               active:text-emerald-800"
-                      
     >
       <a
         href="https://drive.google.com/file/d/1YwT4s3vg5co1AGdN-TABs_qZEhYiF-e6/view?usp=sharing"
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,21 @@ import NavBar from "../components/navbar.js";
 import MyImage2 from "../components/MyImage2.js";
 import MyName from "../components/MyName.js";
 import ResumeDownload from "../components/ResumeDownload.js";
-
+import LinkedinButton from "../components/LinkedinButton.js";
 
 export default function Home() {
   return (
-
     <div>
       <NavBar activeSection="Home" />
       <div className="pl-40 pt-60">
         <div className="">
-          <MyName />
-          <ResumeDownload />
+          <div className="">
+            <MyName />
+          </div>
+          <div className="flex">
+            <LinkedinButton />
+            <ResumeDownload />
+          </div>
         </div>
         <MyImage2 />
       </div>
