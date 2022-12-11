8ea380db13938ec80a8b6c9d6f6f8c9500e41aa2
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-11 21:16:40 +0530

    made all projects images of same size

--- components/LinkedinButton.js
+++ components/LinkedinButton.js
@@ -3,7 +3,7 @@ import {BsLinkedin} from 'react-icons/bs';
 export default function LinkedinButton() {
     return (
       <div
-        className="flex flex-row justify-center mt-20 ml-20 w-max text-lg bg-white px-5 py-3 rounded-lg text-[#0072b1] font-bold
+        className="flex flex-row justify-center mt-20 w-max text-lg bg-white px-5 py-3 rounded-lg text-[#0072b1] font-bold
                    hover:shadow-[0_10px_20px_-15px_#ffffffff] shadow-[0_10px_20px_-15px_#000000ff]">
         <a
           href="https://www.linkedin.com/in/kaustubhdwivedi1729/"
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,98 +3,87 @@ import { AiFillGithub } from "react-icons/ai";
 import NavBar from "../../components/navbar";
 import Header from "../../components/Header";
 
-const Width = 8;
 
 const MyProjects = [
   {
     title: "TMDB Movie App",
     description: "This is my first project",
     tech: "React, Auth0, TailwindCSS",
-    image: "/TMDB.jpg",
+    image: "/projects/tmdb.jpg",
     link: "https://github.com/onlykingKD/movie_db_react_app",
     date: "Dec-2021",
-    width: Width * 22,
   },
   {
     title: "MultiUser Todo App",
     description: "This is my second project",
     tech: "React, Firestore, Firebase Auth",
-    image: "/todo_app.png",
+    image: "/projects/todoapp.jpg",
     link: "https://github.com/onlykingKD/multiuser_todoApp_from_scratch",
     date: "Nov-2021",
-    width: Width * 30,
   },
   {
     title: "Excuser API",
     description: "This is my third project",
     tech: "Node.js, Express.js",
-    image: "/EXCUSER.jpg",
+    image: "/projects/excuser.jpg",
     link: "https://github.com/primeTanM/Excuser/",
     date: "Jan-2022",
-    width: Width * 33,
   },
   {
     title: "FaceMask Detector",
     description: "This is my fourth project",
     tech: "Transfer Learning, Keras, Tensorflow",
-    image: "/facemask.jpg",
+    image: "/projects/facemask.jpg",
     link: "https://github.com/onlykingKD/Face-Mask-Detector",
     date: "Dec-2020",
-    width: Width * 23,
   },
   {
     title: "Frontend Mentor ",
     description: "This is my fifth project",
     tech: "HTML5, CSS3, JavaScript",
-    image: "/frontendmentor.png",
+    image: "/projects/frontend.jpg",
     link: "https://github.com/onlykingKD/FRONTEND-MENTOR-CHALLENGES",
     date: "July-2021",
-    width: Width * 28,
   },
   {
     title: "IP Address Tracker",
     description: "This is my sixth project",
     tech: "HTML5, CSS3, JavaScript",
-    image: "/ipadresstracker.jpg",
+    image: "/projects/ipaddress.jpg",
     link: "https://github.com/onlykingKD/IP-Adress-Tracker",
     date: "Aug-2021",
-    width: Width * 37,
   },
   {
     title: "DOS Attack Detection",
     description: "This is my seventh project",
     tech: "Machine Learning",
-    image: "/DOS.jpg",
+    image: "/projects/DOS.png",
     link: "https://github.com/onlykingKD/DOS-detection-using-ML",
     date: "March-2022",
-    width: Width * 33,
   },
   {
     title: "Lottery Smart Contract",
     description: "This is my eigth project",
     tech: "Solidity, Mocha, Ganache, Truffle",
-    image: "/lottery.jpg",
+    image: "/projects/lottery.png",
     link: "https://github.com/onlykingKD/Lottery-Contract",
     date: "Oct-2022",
-    width: Width * 37,
   },
   {
     title: "Number Guessing Game",
     description: "This is my ninth project",
     tech: "ReactNative, Expo",
-    image: "/numberGuess.png",
+    image: "/projects/guessNumber.jpg",
     link: "https://github.com/onlykingKD/NumberGuessing-ReactNative",
     date: "Sept-2022",
-    width: Width * 37,
   },
   {
     title: "Calculator App",
     description: "This is my tenth project",
     tech: "HTML5, CSS3, JavaScript",
-    image: "/calculator.PNG",
+    image: "/projects/calculator.jpg",
     link: "https://github.com/onlykingKD/Simple-Calculator",
     date: "Aug-2021",
-    width: Width * 33,
   },
 ];
 
@@ -108,14 +97,14 @@ export default function Projects() {
           {MyProjects.map((project) => (
             <div
               key={project.name}
-              className="bg-[#d1fbf5] p-4 mb-8 flex rounded-xl shadow-[0_20px_40px_-15px_#04022fff] hover:shadow-[0_15px_60px_-15px_#4b4b7dff]"
+              className="bg-[#d1fbf5] justify-center items-center p-4 mb-8 flex rounded-xl shadow-[0_20px_40px_-15px_#04022fff] hover:shadow-[0_15px_60px_-15px_#4b4b7dff]"
             >
               <div>
                 <Image
                   src={project.image}
                   alt={project.title}
                   height={0}
-                  width={project.width}
+                  width={300}
                 />
               </div>
               <div className="flex flex-col pl-4 justify-center items-center flex-1">
