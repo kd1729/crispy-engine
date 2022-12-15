66d34c7d32e7a04c86366c0920d3d07d976eec87
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-15 12:46:45 +0530

    removed individual width from Navbar

--- components/MainNavbar.js
+++ components/MainNavbar.js
@@ -4,48 +4,42 @@ const navigationRoutes = [
   {
     name: "Home",
     path: "/",
-    width: "w-12",
   },
   {
     name: "About",
     path: "/about",
-    width: "w-12",
   },
   {
     name: "Projects",
     path: "/projects",
-    width: "w-16",
   },
   {
     name: "Experiences",
     path: "/experience",
-    width: "w-28",
   },
   {
     name: "Blogs",
     path: "/blogs",
-    width: "w-8",
   },
   {
     name: "Contact",
     path: "/contact",
-    width: "w-20",
   },
 ];
 
 function MainNavbar({ activeSection }) {
   return (
-    <div className="navbar-bgcolor sm:flex grid grid-cols-3 gap-2 w-screen justify-center fixed z-10 ">
+    <div className="navbar-bgcolor sm:flex grid grid-cols-3 gap-2 w-screen justify-center items-center fixed z-10 ">
       {navigationRoutes.map((route) => (
         <Link key={route.name} href={route.path} passHref>
           <div
             className={
               [route.name === activeSection
                 ? "text-[#fd5d5d] underline underline-offset-4": "text-white",
-                 "sm:text-2xl text-lg sm:m-8 hover:text-[#e37c73]"].join(" ")
+                 "text-center sm:text-2xl text-lg sm:m-8 hover:text-[#e37c73]"].join(" ")
             }
           >
-            <div className= { [route.width, "font-semibold ease-in-out duration-300"].join(" ")}>
+            <div className="font-semibold ease-in-out duration-300">
               {route.name}
             </div>
           </div>
